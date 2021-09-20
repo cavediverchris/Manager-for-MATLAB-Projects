81c932d81626398077121c8ef176458b2d8d1fe2
chris <chris@DESKTOP-36LNQQM.localdomain>
2021-09-20 09:36:09 +0100

    adding some comments

--- utilities/buildProjectTemplates/buildProjectTemplates.m
+++ utilities/buildProjectTemplates/buildProjectTemplates.m
@@ -1,9 +1,16 @@
+% buildProjectTemplates
+% The purpose of this script is to build the project references (which are
+% templates of MATLAB projects) into Template files which can then be
+% packaged and distributed.
 
-
+% Acquire the current project
 projObj = currentProject;
 
+
+% Get the referenced projects
 refs = projObj.ProjectReferences;
 
+% Iterate over each reference and package it as a template file.
 for r = refs
     exportPath = fullfile(projObj.RootFolder, "toolbox", "source");
     createTemplateFromProject(r.Project, exportPath);
