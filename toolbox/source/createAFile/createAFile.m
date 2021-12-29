dd3a7c3066b6d8f4eb622ba5e4085bca9e423b08
chris <cancerchris88@gmail.com>
2021-12-29 22:24:59 +0000

    the main change is to ensure that all files created as part of this process are added to the project

--- toolbox/source/createAFile/createAFile.m
+++ toolbox/source/createAFile/createAFile.m
@@ -2,33 +2,41 @@
 % This script is used to automate the process of setting up a brand new
 % script according to a naming convention and folder structure.
 
-%% Prepare
-close all;
-clear variables global;
-clc;
+
 %% Request User Input
-
-if ~(exist("answer", 'var') == 1)
-    answer = inputdlg("Enter the title of the file to be created", "New File");
-    answer = answer{:};
-    answer = string(answer);
-end
+answer = inputdlg("Enter the title of the file to be created", "New File");
+answer = answer{:};
+answer = string(answer);
 %% Create container folder
-fcnName = erase(answer, " ");
-%targetDir = fullfile(pwd , fcnName);
-targetDir = pwd;
 
-%% Instantiate the script
-newScriptFilename = fullfile(targetDir, fcnName);
+newFileName = convertSentenceCase(answer, "camel-case");
+
+targetDir = fullfile(pwd , newFileName);
+mkdir(targetDir);
+%targetDir = pwd;
+
+%% Create a script
+% Initialise a new file
+newScriptFilename = fullfile(targetDir, newFileName);
 [fileId] = initialiseFile(newScriptFilename);
 
-%% Populate the file header
-
+% Populate the file header
 initialiseFileHeader(fileId);
 fclose(fileId);
+
+% Add the container folder and new script to the project
+addFolderIncludingChildFiles(projObj, targetDir);
+addPath(projObj, targetDir);
 %% Create Unit Test file
+% A unit test case file is established and added into the tests directory.
+testsDir = fullfile(projObj.RootFolder, "tests");
 
+unitTestFilename = fullfile(testsDir, "test_" + newFileName);
+[fileId] = initialiseFile(unitTestFilename);
+initialiseFileHeader(fileId);
+fclose(fileId);
 
+addFile(projObj, which(unitTestFilename));
 
 %% Create requirements module
 
