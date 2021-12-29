6777c030028e28865bbd594618a6bb9718c61d2e
chris <chris@DESKTOP-36LNQQM.localdomain>
2021-12-29 14:32:45 +0000

    bringing in all copies of all code from other project templates

---
+++ toolbox/source/CreateNewModel/createScript.m
@@ -0,0 +1,20 @@
+%% Create Script
+%
+%   FUNCTION INPUTS
+%       None
+%
+%   FUNCTION OUTPUTS
+%       None
+%
+%   OTHER OUTPUTS
+%       None
+%
+%   LINKS
+%   For further information check out: <a href="matlab:web('https://github.com/cavediverchris/MATLAB-Project-Template-System-Design#create-new-model-createnewmodel')">Git documentation</a>.
+%
+function [] = createScript(filename)
+%CREATESCRIPT Summary of this function goes here
+%   Detailed explanation goes here
+
+StandardisedFileHeader(filename);
+end
