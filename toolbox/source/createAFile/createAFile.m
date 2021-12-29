2f0db417638b15930ed410968da5dbc0cbf8c2c5
chris <cancerchris88@gmail.com>
2021-12-29 21:07:58 +0000

    - added a preparation stage - added some logic so that during early debugging and testing I don't have to keep re-entering a name - provisioned a dummy target directory - also closed the file at the end to prevent conflicts.

--- toolbox/source/createAFile/createAFile.m
+++ toolbox/source/createAFile/createAFile.m
@@ -2,20 +2,29 @@
 % This script is used to automate the process of setting up a brand new
 % script according to a naming convention and folder structure.
 
+%% Prepare
+close all;
+clear variables global;
+clc;
 %% Request User Input
 
-
+if ~(exist("answer", 'var') == 1)
+    answer = inputdlg("Enter the title of the file to be created", "New File");
+    answer = answer{:};
+    answer = string(answer);
+end
 %% Create container folder
-
+fcnName = erase(answer, " ");
+targetDir = fullfile(pwd , fcnName);
 
 %% Instantiate the script
 
-[fileId] = initialiseFile();
+[fileId] = initialiseFile(targetDir);
 
 %% Populate the file header
 
 initialiseFileHeader(fileId);
-
+fclose(fileId);
 %% Create Unit Test file
 
 
