bab9bc1423a4a82297cbf65f08bfc5284da744b5
chris <chris@DESKTOP-36LNQQM.localdomain>
2021-12-29 14:32:03 +0000

    - added arguments block for input validation - added block of code to convert the functionName input into camel case - updated create title section so that it utilises functionName - also added addition section in the interface definition for "other comments" - also added example support section statement

--- toolbox/source/initialiseFileHeader/initialiseFileHeader.m
+++ toolbox/source/initialiseFileHeader/initialiseFileHeader.m
@@ -4,9 +4,27 @@
 
 function [] = initialiseFileHeader(fileId)
 
+arguments
+    fileId mustBeNumeric
+end
+
+%% Case Conversion
+functionName = "TBD";
+if contains(functionName, " ")
+    % CASE: the string that the user gave has spaces
+    % ACTION: Modify the first letter to be upper case
+    capitalize = @(s) upper(extractBefore(s, 2)) + extractAfter(s, 1);
+    componentWords = functionName.split;
+    newWords = arrayfun(capitalize, componentWords);
+    functionName = join(newWords, "");
+end
+
+%% Populate file header
+
 % Create title and description
-fprintf(fileId, '%s', '%%%% Placeholder File Title\n');
-fprintf(fileId, '%s', '%% The purpose of this file is to ...\n');
+fprintf(fileId, '%%%% %s\n', functionName);
+fprintf(fileId, '%s \n', '%% The purpose of this file is to ...');
+fprintf(fileId, '%s \n', '% TODO: Populate the description');
 fprintf(fileId, '%s', '%%\n');
 
 % Create inputs definition section
@@ -17,12 +35,14 @@ fprintf(fileId, '%s', '%%       Description: TBD.\n');
 fprintf(fileId, '%s', '%%       Data Type: TBD.\n');
 fprintf(fileId, '%s', '%%       Dimensions: TBD.\n');
 fprintf(fileId, '%s', '%%       Units: TBD.\n');
+fprintf(fileId, '%s', '%%       Other comments / information: TBD.\n');
 fprintf(fileId, '%s', '\n');
 fprintf(fileId, '%s', '%%   Input 2: <<INPUT 1 NAME>>');
 fprintf(fileId, '%s', '%%       Description: TBD.\n');
 fprintf(fileId, '%s', '%%       Data Type: TBD.\n');
 fprintf(fileId, '%s', '%%       Dimensions: TBD.\n');
 fprintf(fileId, '%s', '%%       Units: TBD.\n');
+fprintf(fileId, '%s', '%%       Other comments / information: TBD.\n');
 
 % Create outputs definition section
 fprintf(fileId, '%s', '%%%% OUTPUTS\n');
@@ -41,6 +61,8 @@ fprintf(fileId, '%s', '%%       Units: TBD.\n');
 
 % Create support section
 fprintf(fileId, '%s', '%%%% SUPPORT\n');
+fprintf(fileId, '%s \n', '% For further information check out: <a href="matlab:web("https://github.com/cavediverchris/")">Git documentation</a>.'); 
+fprintf(fileId, '%s \n', '% TODO: Populate any relevent external links ... ');
 
 end
 
