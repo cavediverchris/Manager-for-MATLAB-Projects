282b4fbf001e8953a70999a219fafd56eb59da0f
chris <cancerchris88@gmail.com>
2021-12-30 07:13:05 +0000

    created basic test to exercise the planned behaviour of the createAFile script

--- tests/test_createAFile.m
+++ tests/test_createAFile.m
@@ -1,23 +1,34 @@
-classdef test_createAFile
-    %TEST_CREATEAFILE Summary of this class goes here
-    %   Detailed explanation goes here
-    
-    properties
-        Property1
-    end
-    
-    methods
-        function obj = test_createAFile(inputArg1,inputArg2)
-            %TEST_CREATEAFILE Construct an instance of this class
-            %   Detailed explanation goes here
-            obj.Property1 = inputArg1 + inputArg2;
+classdef test_createAFile < matlab.unittest.TestCase
+    methods(Test)
+        function confirmAScriptAndTestIsMade(testCase)
+            %% Description
+            % The purpose of this test is to ensure that the createAFile
+            % script will correctly set up a new MATLAB script and a unit
+            % test case
+            %% Test Setup
+            newFileName = "aBasicTest";
+            projObj = currentProject;
+            %% Test Execution
+            createAFile(newFileName);
+            %% Test Verification
+            % We want to check that:
+            % - a new M-file has been created
+            % - a test class has been created
+            fcnExists = exist(which(newFileName), 'file');
+            testExists = exist(which("test_" + newFileName), 'file');
+            
+            
+            expSolution = 2;
+            testCase.verifyEqual(fcnExists,expSolution);
+            testCase.verifyEqual(testExists,testExists);
+            
+            %% Test Teardown
+            % Remove the files created from the project
+            removeFile(projObj, which(newFileName));
+            removeFile(projObj, fullfile(projObj.RootFolder, newFileName) );
+%            removePath(projObj, fullfile(projObj.RootFolder, newFileName) );
+            removeFile(projObj, which("test_" + newFileName));
         end
-        
-        function outputArg = method1(obj,inputArg)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
-        end
+
     end
 end
-
