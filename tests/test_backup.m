457870d851c97e1ca2802efa2a7bdb3eecab9404
cavediverchris <cancerchris88+github@gmail.com>
2022-01-01 10:45:00 +0000

    updated test case to exercise the back up function

--- tests/test_backup.m
+++ tests/test_backup.m
@@ -1,35 +1,34 @@
-%% TBD
-% The purpose of this file is to ...
-% TODO: Populate the description
-%%%% INPUTS
-% This function accepts the following inputs:
-%   Input 1: <<INPUT 1 NAME>>
-%       Description: TBD.
-%       Data Type: TBD.
-%       Dimensions: TBD.
-%       Units: TBD.
-%       Other comments / information: TBD.
-%
-%   Input 2: <<INPUT 1 NAME>>
-%       Description: TBD.
-%       Data Type: TBD.
-%       Dimensions: TBD.
-%       Units: TBD.
-%       Other comments / information: TBD.
-%
-%% OUTPUTS
-% This function accepts the following inputs:
-%   Output 1: <<OUTPUT 1 NAME>>
-%       Description: TBD.
-%       Data Type: TBD.
-%       Dimensions: TBD.
-%       Units: TBD.
-%
-%   Output 2: <<OUTPUT 1 NAME>>
-%       Description: TBD.
-%       Data Type: TBD.
-%       Dimensions: TBD.
-%       Units: TBD.
-%% SUPPORT
-% For further information check out: <a href="matlab:web("https://github.com/cavediverchris/")">Git documentation</a>.
-% TODO: Populate any relevent external links ... 
+classdef test_backup < matlab.unittest.TestCase
+    methods(Test)
+        % What if the desintation is invalid?
+        % What about if the destination already has a backup file?
+        function confirmCamelCase(testCase)
+            %% Description
+            % The purpose of this test is to ensure that the backup
+            % function creates a backup when the destination is valid
+            %% Test Setup
+            projObj = currentProject;
+            
+            destination = fullfile(projObj.RootFolder, "backupsFolder");
+            mkdir(destination);
+            %% Test Execution
+            backup(destination);
+            %% Test Verification
+            % We want to check that the directory contains a backup, if it
+            % does, there will be three entries in dirContents
+            
+            dirContents = dir(destination);
+
+            expSolution = 3;
+            testCase.verifyEqual(length(dirContents),expSolution);
+            
+            %% Test Teardown
+            status = rmdir(destination, 's');
+            
+            % Then verify that the directory was actually deleted
+            testCase.verifyEqual(status, true);
+        end
+    
+        
+    end
+end
