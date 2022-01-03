ded21607ec651fcd92d5352301ca5b5166874753
chris <chris@DESKTOP-36LNQQM.localdomain>
2022-01-03 16:29:59 +0000

    added a basic test

--- tests/test_initialiseFileHeader.m
+++ tests/test_initialiseFileHeader.m
@@ -1,23 +1,20 @@
-classdef test_initialiseFileHeader
-    %TEST_INITIALISEFILEHEADER Summary of this class goes here
-    %   Detailed explanation goes here
-    
-    properties
-        Property1
-    end
-    
-    methods
-        function obj = test_initialiseFileHeader(inputArg1,inputArg2)
-            %TEST_INITIALISEFILEHEADER Construct an instance of this class
-            %   Detailed explanation goes here
-            obj.Property1 = inputArg1 + inputArg2;
-        end
-        
-        function outputArg = method1(obj,inputArg)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
-        end
-    end
+classdef test_initialiseFileHeader < matlab.unittest.TestCase
+    methods(Test) 
+       function testAbleToInitialiseFile(testCase)
+           %% Description
+           % The purpose of this test is to ensure that the a file can be
+           % initialised.
+           %% Setup
+           newFilename = "test";
+           fullFilename = fullfile(pwd, newFilename);
+           [fileID] = initialiseFile(fullFilename);
+           %% Exercise
+           initialiseFileHeader(fileID);
+           %% Verify
+           testCase.verifyEqual(exist(fullFilename, 'file'), 2)
+           %% Teardown
+           fclose(fileID);
+           delete(fullFilename + ".m");
+       end
+    end % methods (Test)
 end
-
