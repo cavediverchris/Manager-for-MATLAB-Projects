82f3e24c6e85ca5fff530e8a3098d15e77a06c1d
cavediverchris <cancerchris88+github@gmail.com>
2022-01-07 12:57:30 +0000

    updated header documentation

--- toolbox/source/convertSentenceCase/convertSentenceCase.m
+++ toolbox/source/convertSentenceCase/convertSentenceCase.m
@@ -1,3 +1,48 @@
+%% Convert Sentence Case
+% The purpose of this file is to take an input string that is a sentence
+% (i.e. a series of words with spaces between them) and to then convert
+% that sentence into a single "word" where the case convention is as
+% specified by the user.
+%
+% This utility currently supports the following case conversions:
+%
+% - Camel Case
+%
+%% INPUTS
+% This function accepts the following inputs:
+%   Input 1: inputString
+%       Description: This input is the string that contains the sentence
+%       that is to be modified based upon the case convention specified by
+%       the second input.
+%       Data Type: String.
+%       Dimensions: 1x1.
+%       Units: N/A.
+%
+%   Input 2: sentenceCase
+%       Description: This input specifies the case that the user wishes to
+%       have the sentence converted based upon.
+%       Data Type: String.
+%       Dimensions: 1x1.
+%       Units: N/A.
+%
+%% OUTPUTS
+% This function accepts the following inputs:
+%   Output 1: formattedSentence
+%       Description: This output is a string based upon the input defined
+%       by inputString after being converted based upon the convention
+%       defined in sentenceCase.
+%       Data Type: string.
+%       Dimensions: 1x1.
+%       Units: N/A.
+%
+%% SUPPORT
+% This function works based upon the string data type. You can learn more
+% about Strings from the <a href="matlab:web('https://uk.mathworks.com/help/matlab/ref/string.html?s_tid=doc_ta')">documentation.</a>.
+% 
+% You can also report bugs or suggestions for improvements in the "issues"
+% section of <a href="matlab:web('https://github.com/cavediverchris/Manager-for-MATLAB-Projects/issues')">Github.</a>.
+
+%% MAIN
 function [formattedSentence] = convertSentenceCase(inputString,sentenceCase)
 sentenceCase = char(sentenceCase);
 
