d5919a43cde5c4a360de2f8b2ed3d155872eca28
cavediverchris <cancerchris88+github@gmail.com>
2022-01-08 12:31:39 +0000

    identified a bug where the script was not properly handling a specified file name with no extension.

--- toolbox/source/initialiseFile/initialiseFile.m
+++ toolbox/source/initialiseFile/initialiseFile.m
@@ -36,15 +36,15 @@ function [fileID] = initialiseFile(fullFilePath)
 
 [path, name, ext] = fileparts(fullFilePath);
 
-if isempty(ext)
+if strcmp(ext, "")
     % CASE: The user did not include a file extension in the name
     % ACTION: Set to m
     ext = "m";
-    fullFilePath = fullfile(path, name + ext);
+    fullFilePath = fullfile(path, name + "." + ext);
 else
     % CASE: The user did provide a file extension
     % ACTION: Use the provided extension
-     fullFilePath = fullfile(path, name + ext);
+     fullFilePath = fullfile(path, name + "." + ext);
 end
 %% Initialise file
 
