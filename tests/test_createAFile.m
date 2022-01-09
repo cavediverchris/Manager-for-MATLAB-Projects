51cff02a1b83a8847b7460e2045454a56946b0b1
cavediverchris <cancerchris88+github@gmail.com>
2022-01-09 11:10:31 +0000

    test modified based on the basis that createAFile is now creating a single script

--- tests/test_createAFile.m
+++ tests/test_createAFile.m
@@ -11,35 +11,27 @@
 %% MAIN
 classdef test_createAFile < matlab.unittest.TestCase
     methods(Test)
-        function confirmAScriptAndTestIsMade(testCase)
+        function confirmScriptIsMade(testCase)
             %% Description
             % The purpose of this test is to ensure that the createAFile
-            % script will correctly set up a new MATLAB script and a unit
-            % test case
+            % script will correctly set up a new MATLAB script
             %% Test Setup
-            newFileName = "aBasicTest";
-            projObj = currentProject;
+            newFileName = "aBasicTest.m";
+            newFileName = fullfile(pwd, newFileName);
             %% Test Execution
             createAFile(newFileName);
             %% Test Verification
             % We want to check that:
             % - a new M-file has been created
-            % - a test class has been created
             fcnExists = exist(which(newFileName), 'file');
-            testExists = exist(which("test_" + newFileName), 'file');
             
             
             expSolution = 2;
             testCase.verifyEqual(fcnExists,expSolution);
-            testCase.verifyEqual(testExists,expSolution);
             
             %% Test Teardown
             % Remove the files created from the project
-            removeFile(projObj, which(newFileName));
-            removePath(projObj, fullfile(projObj.RootFolder, newFileName) );
-            removeFile(projObj, fullfile(projObj.RootFolder, newFileName) );
-            
-            removeFile(projObj, which("test_" + newFileName));
+            delete(which(newFileName));
         end
 
     end
