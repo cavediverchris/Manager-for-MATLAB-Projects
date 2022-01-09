c6fb6229d7b29cbc2a9aff3ba911cec49b3fa8e8
cavediverchris <cancerchris88+github@gmail.com>
2022-01-09 11:10:09 +0000

    this has now been simplified to creating just a single M-file only. (new function for creating a package will create more infrastructure etc.)

--- toolbox/source/createAFile/createAFile.m
+++ toolbox/source/createAFile/createAFile.m
@@ -1,6 +1,6 @@
 %% Create A File
 % This script is used to automate the process of setting up a brand new
-% script according to a naming convention and folder structure.
+% script according to a naming convention and with a template file header
 %% INPUTS
 % This function accepts the following inputs:
 %   Input 1: fileTitle
@@ -22,57 +22,13 @@
 %% MAIN
 function [] = createAFile(fileTitle)
 
-%% Determine if a path has been provided
-% If a path has been provided then the user knows where they want the file
-% to be created
-
-[path, name, ext] = fileparts(fileTitle);
-
-if isempty(path)
-    % CASE: A path was not provided
-    % ACTION: Set a default path
-    targetDir = fullfile(pwd);
-    name = fileTitle;
-else
-    % CASE: User specified a path
-    % ACTION: Set the path appropriately
-    targetDir = fullfile(path);
-    name = name;
-end
-
-%% Set the file name according to the naming convention
-
-newFileName = convertSentenceCase(name, "camel-case");
-%% Create container folder
-
-
-
-targetDir = fullfile(targetDir , newFileName);
-mkdir(targetDir);
-%targetDir = pwd;
 
 %% Create a script
 % Initialise a new file
-newScriptFilename = fullfile(targetDir, newFileName);
-[fileId] = initialiseFile(newScriptFilename);
+[fileId] = initialiseFile(fileTitle);
 
 % Populate the file header
 initialiseFileHeader(fileId);
 fclose(fileId);
-
-% Add the container folder and new script to the project
-projObj = currentProject;
-addFolderIncludingChildFiles(projObj, targetDir);
-addPath(projObj, targetDir);
-%% Create Unit Test file
-% A unit test case file is established and added into the tests directory.
-testsDir = fullfile(projObj.RootFolder, "tests");
-
-unitTestFilename = fullfile(testsDir, "test_" + newFileName);
-[fileId] = initialiseFile(unitTestFilename);
-initialiseFileHeader(fileId);
-fclose(fileId);
-
-addFile(projObj, which(unitTestFilename));
 
 end
