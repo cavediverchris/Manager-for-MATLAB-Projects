83d68902fb311d49f6c22d7fce31174e1ab3d031
chris <chris@DESKTOP-36LNQQM.localdomain>
2022-01-18 16:10:30 +0000

    built up test

--- tests/test_buildToolbox.m
+++ tests/test_buildToolbox.m
@@ -7,4 +7,34 @@
 % 
 % You can also report bugs or suggestions for improvements in the "issues"
 % section of <a href="matlab:web('https://github.com/cavediverchris/Manager-for-MATLAB-Projects/issues')">Github.</a>.
+
 %% MAIN
+classdef test_buildToolbox < matlab.unittest.TestCase
+    methods(Test)
+        function confirmBuildToolbox(testCase)
+            %% Description
+            % The purpose of this test is to ensure that the build toolbox
+            % script generates a MATLAB Toolbox file
+            %% Test Setup
+            
+
+            %% Test Execution
+            buildToolbox;
+            %% Test Verification
+            % We want to check that the output from convertSentenceCase
+            % function is as expected
+
+            tbxName = "Manager for MATLAB Projects" + ".mltbx";
+            result = exist(which(tbxName), 'file');
+            
+            expSolution = 2;
+            testCase.verifyEqual(result,expSolution);
+            
+            %% Test Teardown
+            delete(which(tbxName));
+
+        end
+    
+        
+    end
+end
