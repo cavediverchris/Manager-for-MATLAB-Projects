45e91fead804d1faed6671a40374b2fddfa7812e
chris <chris@DESKTOP-36LNQQM.localdomain>
2022-01-18 14:46:32 +0000

    added setting the project labels to each file

--- toolbox/source/createAModelPackage/createAModelPackage.m
+++ toolbox/source/createAModelPackage/createAModelPackage.m
@@ -81,18 +81,27 @@ projObj = currentProject;
 addFile(projObj, targetDir);
 addPath(projObj, targetDir);
 
+myFile = findFile(projObj, targetDir);
+myFile.addLabel("Classification", "Design");
 %% Create the model
 newModelFilename = fullfile(targetDir, newModelName + ext);
 createAModel(newModelFilename);
 addFile(projObj, newModelFilename);
 
+myFile = findFile(projObj, newModelFilename);
+myFile.addLabel("Classification", "Design");
 %% Create requirements 
 % In this section we want to create a requirements file alongside the newly
 % created MATLAB script
 reqtsFile = fullfile(targetDir, "reqts_" + newModelName);
 createReqtsModule(reqtsFile);
 %addFile(projObj, reqtsFile);
+%myFile = findFile(projObj, newModelFilename);
+%myFile.addLabel("Classification", "Artifact");
 
 %% Create Test Harness
 [testHarnessFilename] = createAModelTestHarness(newModelFilename);
 addFile(projObj, which(testHarnessFilename));
+
+myFile = findFile(projObj, testHarnessFilename);
+myFile.addLabel("Classification", "Test");
