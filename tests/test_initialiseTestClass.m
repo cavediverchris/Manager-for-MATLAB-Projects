c4cb20987d44e2e37f9228e80a82b259b59e92d4
cavediverchris <cancerchris88+github@gmail.com>
2022-01-23 07:49:36 +0000

    updated test case to test that a file has been properly initialised as a test case by detecting if it is now a class

--- tests/test_initialiseTestClass.m
+++ tests/test_initialiseTestClass.m
@@ -1,43 +1,38 @@
-%% test_initialiseTestClass
-% The purpose of this file is to ...
-% TODO: Populate the description
-%% INPUTS
-% This function accepts the following inputs:
-%   Input 1: <<INPUT 1 NAME>>
-%       Description: TBD.
-%       Data Type: TBD.
-%       Dimensions: TBD.
-%       Units: TBD.
-%       Other comments / information: TBD.
-%
-%   Input 2: <<INPUT 1 NAME>>
-%       Description: TBD.
-%       Data Type: TBD.
-%       Dimensions: TBD.
-%       Units: TBD.
-%       Other comments / information: TBD.
-%
-%% OUTPUTS
-% This function accepts the following inputs:
-%   Output 1: <<OUTPUT 1 NAME>>
-%       Description: TBD.
-%       Data Type: TBD.
-%       Dimensions: TBD.
-%       Units: TBD.
-%
-%   Output 2: <<OUTPUT 1 NAME>>
-%       Description: TBD.
-%       Data Type: TBD.
-%       Dimensions: TBD.
-%       Units: TBD.
+%% Unit Test Case
+% The purpose of this file is to use the MATLAB Unit Testing framework in
+% order to automate the testing of the function under test.
 %% SUPPORT
-% For further information check out: <a href="matlab:web('https://github.com/cavediverchris/')">Git documentation</a>.
-% TODO: Populate any relevent external links ... 
+% This test script has been built using the MATLAB Unit Testing framework,
+% you can get further information on this from the <a href="matlab:web('https://uk.mathworks.com/help/matlab/matlab-unit-test-framework.html')">support page.</a>.
+% 
 % You can also report bugs or suggestions for improvements in the "issues"
 % section of <a href="matlab:web('https://github.com/cavediverchris/Manager-for-MATLAB-Projects/issues')">Github.</a>.
-% Test harness for initialiseTestClass
-%% Template Test 1: Insert Test Name
 
-inputData = 1;
-functionOuput = functionName(inputData);
-assert(functionOutput == criteria, 'Error message if false');
+%% MAIN
+classdef test_initialiseTestClass < matlab.unittest.TestCase
+    methods(Test) 
+       function testAbleToInitialiseFile(testCase)
+           %% Description
+           % The purpose of this test is to ensure that we can create a
+           % MATLAB unit test case file
+           %% Setup
+           newFilename = "myTest";
+           fullFilename = fullfile(pwd, newFilename + ".m");
+           
+           % First create an M-file with header
+           createAFile(fullFilename);
+           
+           % Extend the test case with some boiler plate material to get the user
+           % started with unit test cases
+           fileId = fopen(fullFilename, 'a');
+           
+           %% Exercise
+           initialiseTestClass(fileId);
+           %% Verify
+           testCase.verifyEqual(exist(newFilename, 'class'), 8);
+           %% Teardown
+           fclose(fileId);
+           delete(which(fullFilename), 's');
+       end
+    end % methods (Test)
+end
